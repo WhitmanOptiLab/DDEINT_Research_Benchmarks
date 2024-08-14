5b347f2329a159c20f4d2b48a7dfa101dea3b6d9
TMahlatini <mahlatit@whitman.edu>
2024-08-14 13:04:54 -0700

    refactored to match new ddeint interface

--- test/2D_Heat_Equation/heat_equation.cpp
+++ test/2D_Heat_Equation/heat_equation.cpp
@@ -153,7 +153,7 @@ int main() {
     // Initialize the state
     initialize_y(nx_loc, ny_loc, dx, dy, init_cond_laplacian);
     
-    std::vector<double> output, time;
+    std::vector<std::vector<double>> output; 
     std::vector<std::vector<double>> snapshots;
     snapshots.reserve(20);
 
@@ -162,50 +162,56 @@ int main() {
     double dt = 0.05; // Time step
     double tf = 1.0; // Final time
     int num_steps = tf / dt; // Number of time steps
+    int num_snapshots = 20; // Number of snapshots to take
+    double t_start = 0.0;
+    double t_end = 1.0;
 
-    for (int step = 0; step < num_steps; ++step) {
-        double t_start = step * dt;
-        double t_end = (step + 1) * dt;
+     output = test_laplacian.run(20,t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, false);
 
-        if (step == 0) {
-            // Initialize the integrator at the first step
-            output = test_laplacian.run(t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, false);
-            snapshots.push_back(output);
-            time.push_back(test_laplacian.get_t());
-        } else {
-            // Continue the integration for subsequent steps
-           output = test_laplacian.continue_integration(t_end, 50000,  false);
-           snapshots.push_back(output);
-           time.push_back(test_laplacian.get_t());
-        }
-    }
+    // for (int step = 0; step < num_steps; ++step) {
+    //     double t_start = step * dt;
+    //     double t_end = (step + 1) * dt;
+
+    //     if (step == 0) {
+    //         // Initialize the integrator at the first step
+    //         output = test_laplacian.run(t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, false);
+    //         snapshots.push_back(output);
+    //         time.push_back(test_laplacian.get_t());
+    //     } else {
+    //         // Continue the integration for subsequent steps
+    //        output = test_laplacian.continue_integration(t_end, 50000,  false);
+    //        snapshots.push_back(output);
+    //        time.push_back(test_laplacian.get_t());
+    //     }
+    // }
 
 
     auto end_time = std::chrono::high_resolution_clock::now();
     double execution_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
     std::cout << "Execution time: " << execution_time << " milliseconds\n";
-    //output.insert(output.begin(), tf);
-    while (!output.empty()) {
-        output.pop_back();
-    }
-    // Calculate the rms of each snapshot
-    int col_width = 25; 
-    std::cout << std::left;
-    std::cout << std::setw(col_width) << "time" << std::setw(col_width) << "||u||_rms " << std::endl;
-    for (size_t i = 0; i < snapshots.size(); ++i) {
-        double rms = calculate_rms(snapshots[i]);
-        output.push_back(rms);
-        std::cout << std::scientific;
-        std::cout << std::setprecision(16);
-        std::cout << std::setw(col_width) << time[i] << std::setw(col_width) << rms << std::endl;
+
+    // //output.insert(output.begin(), tf);
+    // while (!output.empty()) {
+    //     output.pop_back();
+    // }
+    // // Calculate the rms of each snapshot
+    // int col_width = 25; 
+    // std::cout << std::left;
+    // std::cout << std::setw(col_width) << "time" << std::setw(col_width) << "||u||_rms " << std::endl;
+    // for (size_t i = 0; i < snapshots.size(); ++i) {
+    //     double rms = calculate_rms(snapshots[i]);
+    //     output.push_back(rms);
+    //     std::cout << std::scientific;
+    //     std::cout << std::setprecision(16);
+    //     std::cout << std::setw(col_width) << time[i] << std::setw(col_width) << rms << std::endl;
 
         
-    }
+    // }
      
 
     
-    write_to_csv(time, output, "data/heat_equation_output.csv");
+    //write_to_csv(time, output, "data/heat_equation_output.csv");
 
     return 0;
 }
