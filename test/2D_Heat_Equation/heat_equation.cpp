f89d560785f12249b4695c9084033234bd964e2a
TMahlatini <mahlatit@whitman.edu>
2024-08-15 08:38:19 -0700

    added callback function

--- test/2D_Heat_Equation/heat_equation.cpp
+++ test/2D_Heat_Equation/heat_equation.cpp
@@ -95,26 +95,17 @@ void initialize_y(size_t nx_loc, size_t ny_loc, double dx, double dy, std::vecto
 }
 
 
-void write_to_csv(const std::vector<double>& time ,const std::vector<double>& data, const std::string& filename) {
-    std::ofstream file(filename);
 
-    if (!file.is_open()) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return; 
-
+// should this be a member function of DDEint_dopri_5?
+void log_step(double time, const std::vector<double>& state, std::string filename) {
+    std::ofstream file(filename, std::ios::app);
+    file << time << ",";
+    for (double val : state) {
+        file << val << ",";
     }
-
-    file << "time,||u||_rms" << std::endl;
-    for (size_t i = 0; i < time.size(); ++i) {
-        file << time[i] << "," << data[i] << std::endl;
-    }
-
-    
-    
-    file << std::endl;
-
-    file.close();
+    std::endl(file);
 }
+
 
 double calculate_rms(const std::vector<double>& values) {
     double sum_of_squares = 0.0;
@@ -125,12 +116,7 @@ double calculate_rms(const std::vector<double>& values) {
     return sqrt(mean);
 }
 
-//update DDE library so that it does  not return a vector of all the snapshots but only the last snapshot of the time it finished. Outside of the ring buffer we are not saving any other snapshots.
-//optimize m_output array. check to see where its being updated. 
-// change test case to take snapshots 1/20th for a 1sec. basically call run 20 times and get the last snapshot of each run
 
-//check the DDE library to see if its not saving all the history in the ringbuffer. we not storing any data in the ringbuffer. 
-//parameters for ringbuffer should basically be zero in this test case.
 
 int main() {
     // Dummy prehistory function (returns 0.0 for all inputs)
@@ -154,64 +140,25 @@ int main() {
     initialize_y(nx_loc, ny_loc, dx, dy, init_cond_laplacian);
     
     std::vector<std::vector<double>> output; 
-    std::vector<std::vector<double>> snapshots;
-    snapshots.reserve(20);
 
+    
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    double dt = 0.05; // Time step
-    double tf = 1.0; // Final time
-    int num_steps = tf / dt; // Number of time steps
-    int num_snapshots = 20; // Number of snapshots to take
     double t_start = 0.0;
     double t_end = 1.0;
 
-     output = test_laplacian.run(20,t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, false);
+     //output = test_laplacian.run(20,t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, false);
 
-    // for (int step = 0; step < num_steps; ++step) {
-    //     double t_start = step * dt;
-    //     double t_end = (step + 1) * dt;
-
-    //     if (step == 0) {
-    //         // Initialize the integrator at the first step
-    //         output = test_laplacian.run(t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, false);
-    //         snapshots.push_back(output);
-    //         time.push_back(test_laplacian.get_t());
-    //     } else {
-    //         // Continue the integration for subsequent steps
-    //        output = test_laplacian.continue_integration(t_end, 50000,  false);
-    //        snapshots.push_back(output);
-    //        time.push_back(test_laplacian.get_t());
-    //     }
-    // }
-
+     test_laplacian.run(20, t_start, t_end, init_cond_laplacian, 0.005, 0.0005, 50000, 1e-10, 1e-5, log_step, false);
+     
 
     auto end_time = std::chrono::high_resolution_clock::now();
     double execution_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
     std::cout << "Execution time: " << execution_time << " milliseconds\n";
-
-    // //output.insert(output.begin(), tf);
-    // while (!output.empty()) {
-    //     output.pop_back();
-    // }
-    // // Calculate the rms of each snapshot
-    // int col_width = 25; 
-    // std::cout << std::left;
-    // std::cout << std::setw(col_width) << "time" << std::setw(col_width) << "||u||_rms " << std::endl;
-    // for (size_t i = 0; i < snapshots.size(); ++i) {
-    //     double rms = calculate_rms(snapshots[i]);
-    //     output.push_back(rms);
-    //     std::cout << std::scientific;
-    //     std::cout << std::setprecision(16);
-    //     std::cout << std::setw(col_width) << time[i] << std::setw(col_width) << rms << std::endl;
-
-        
-    // }
-     
+ 
 
     
-    //write_to_csv(time, output, "data/heat_equation_output.csv");
 
     return 0;
 }
